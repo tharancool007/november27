3895c1dea4bd32cd1eeca685ec98cf0d702da6b8
tharancool007 <tharancool007@gmail.com>
2022-12-07 16:12:32 +0530

    new updated copy

--- src/test/java/org/BaseandPojo/PojoHomebase.java
+++ src/test/java/org/BaseandPojo/PojoHomebase.java
@@ -19,11 +19,6 @@ public class PojoHomebase extends Baseclass {
 	@FindAll({
 		@FindBy(xpath="(//span[@class='responsiveAccountHeader_openAccountPanelText'])[1]"),
 
-		@FindBy(xpath="//span[text()='Acco]"),
-
-		@FindBy(xpath="(//span[text()='Account'])[2]"),
-		@FindBy(xpath="(//span[text()='Account'])[3]"),
-
 		@FindBy(xpath="(//span[text()='Account'])[1]")
 		
 	})private WebElement loginoption;
